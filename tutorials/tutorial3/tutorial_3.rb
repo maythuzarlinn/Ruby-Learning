b012e8771b17273b76f71de196b9eb4efe339d5e
May Thu <maythuzarlinn96@gmail.com>
2022-02-17 20:54:07 +0630

    feature/tutorial_03 tutorial 03  Updated Calculator App.

--- tutorials/tutorial3/tutorial_3.rb
+++ tutorials/tutorial3/tutorial_3.rb
@@ -1,37 +1,13 @@
-puts "Enter a number to calculate: "
-num1 = gets.chomp().to_f
-
-puts "Enter operator to calculate (Eg. + , - ,*, /,% ): "
-op = gets.chomp()
-
-puts "Enter another number to calculate: "
-num2 = gets.chomp().to_f
-
-if op == "+"
-  puts (num1 + num2)
-elsif op == "-"
-  puts (num1 - num2)
-elsif op == "*"
-  puts (num1 * num2)
-elsif op == "/"
-  puts (num1 / num2)
-elsif op == "%"
-  puts (num1 % num2)
-else
-  puts "Invalid operator"
-end
-puts "Would you like to calculate another operation?(Please enter yes if you want to continue : yes (or) no ) "
-con = gets.chomp()
-
+puts "Welcome to Maye Calculator!!!"
+con = "yes"
 while con == "yes"
-  puts "Enter a number to calculate: "
+  puts "Please, enter a number to calculate: "
   num1 = gets.chomp().to_f
-  puts "Enter operator to calculate (Eg. + , - ,*, /,% ): "
+  puts "Please, enter operator to calculate (Eg. + , - ,*, /,% ): "
   op = gets.chomp()
-  puts "Enter another number to calculate: "
+  puts "Please,  enter another number to calculate: "
   num2 = gets.chomp().to_f
   
-
   if op == "+"
     puts (num1 + num2)
   elsif op == "-"
@@ -48,4 +24,4 @@ while con == "yes"
   puts "Would you like to calculate another operation?(Please enter yes if you want to continue : yes (or) no ) "
   con = gets.chomp()
 end
-puts "Thanks for using our program.We stop the program."
+puts "Thanks for using our calculator.We stopped the system."
